3901cee1210e472fbd28f8afda00105e2f884e77
Ivan <Kishkan91@gmail.com>
2019-06-21 23:31:56 +0400

    - added 4th hello world

--- src/com/kishkan91/Main.java
+++ src/com/kishkan91/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("The first Hello world!");
         System.out.println("The second Hello world!");
         System.out.println("The third Hello world!");
+        System.out.println("The 4th hello world!");
     }
 }
