acc594671b16c9a48cc66abc38878c6c8ab86da4
Ivan <Kishkan91@gmail.com>
2019-06-21 16:51:56 +0400

    - Third hello world added - First hello world edited

--- src/com/kishkan91/Main.java
+++ src/com/kishkan91/Main.java
@@ -2,7 +2,8 @@ package com.kishkan91;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("The first Hello world!");
         System.out.println("The second Hello world!");
+        System.out.println("The third Hello world!");
     }
 }
